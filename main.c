a5191ee3374824029d36aacb21f3458cf7362709
guyInTheChair-8bit <reddyvenkatahemanth307@gmail.com>
2026-01-15 18:34:34 +0530

    Added backspace, enter key, and dynamic text wrapping

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #include <string.h>
 
 const char* TITLE = "GUY-EDITOR";
-const int SCREEN_WIDTH = 1920;
-const int SCREEN_HEIGHT = 1920;
+int winWidth = 1920;
+int winHeight = 1920;
 const int ptsize = 48;
 
 void toggle_fullscreen (SDL_Window* window, int is_fullscreen);
-void drawText(const char* text, int x, int y, TTF_Font* font, SDL_Color* color, SDL_Renderer* renderer);
+void drawText(const char* text, int x, int y, TTF_Font* font, SDL_Color* color, SDL_Renderer* renderer, int winWidth);
 
 int main (int argc, char** argv) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -23,7 +23,7 @@ int main (int argc, char** argv) {
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    SDL_Window* window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winWidth, winHeight, 0);
 
     if (window == NULL) {
         printf("Window creation failed: %s\n", SDL_GetError());
@@ -54,25 +54,31 @@ int main (int argc, char** argv) {
     while (is_running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) is_running = 0;
-            if (event.type == SDL_TEXTINPUT) {
+            else if (event.type == SDL_TEXTINPUT) {
                 if (strlen(strBuffer) + strlen(event.text.text) < 1024) {
                     strcat(strBuffer, event.text.text);
                 } 
             }
-            if (event.type == SDL_KEYDOWN) {
+            else if (event.type == SDL_KEYDOWN) {
                 if (event.key.keysym.sym == SDLK_f) { 
                     if (event.key.keysym.mod & KMOD_CTRL || event.key.keysym.mod & KMOD_GUI) {
                         is_fullscreen = !is_fullscreen;
                         toggle_fullscreen(window, is_fullscreen);
                     }
                 }
-                if (event.key.keysym.sym == SDLK_ESCAPE) is_running = 0;
+                else if (event.key.keysym.sym == SDLK_BACKSPACE) strBuffer[strlen(strBuffer) - 1] = '\0';
+                else if (event.key.keysym.sym == SDLK_RETURN) strBuffer[strlen(strBuffer)] = '\n';
+                else if (event.key.keysym.sym == SDLK_ESCAPE && is_fullscreen) {
+                    is_fullscreen = !is_fullscreen;
+                    toggle_fullscreen(window, 0);
+                }
             }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
-        drawText(strBuffer, 100, 100, font, &white, renderer);
+        SDL_GetWindowSize(window, &winWidth, &winHeight);
+        drawText(strBuffer, 100, 100, font, &white, renderer, winWidth);
         SDL_RenderPresent(renderer);
     }
 
@@ -88,9 +94,9 @@ void toggle_fullscreen (SDL_Window* window, int is_fullscreen) {
     else SDL_SetWindowFullscreen(window, 0);
 }
 
-void drawText(const char* text, int x, int y, TTF_Font* font, SDL_Color* color, SDL_Renderer* renderer) {
+void drawText(const char* text, int x, int y, TTF_Font* font, SDL_Color* color, SDL_Renderer* renderer, int winWidth) {
 
-    SDL_Surface* surfaceMsg = TTF_RenderText_Blended_Wrapped(font, text, *color, SCREEN_WIDTH - 200);
+    SDL_Surface* surfaceMsg = TTF_RenderText_Blended_Wrapped(font, text, *color, winWidth - 200);
     SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surfaceMsg);
     SDL_FreeSurface(surfaceMsg);
     
